c2f5de4ac3682afc80ca7313f2c887c055858594
DESKTOP-O4NN41P\82109 <khb4130@naver.com>
2024-06-07 21:55:00 +0900

    route 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import Header from "./component/header";
-import "./css/header.css";
+import "./css/header_footer.css";
+import Footer from "./component/footer";
+import Contents1 from "./component/contents1";
+import Contents2 from "./component/contents2";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+
 function App() {
   return (
     <>
-      <Header />
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/component" element={<Contents1 />} />
+          <Route exact path="/component2" element={<Contents2 />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
 }
